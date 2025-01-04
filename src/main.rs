3d79c769c616437a478fe08229c3451e910a6d18
KintalLam <KintalLam@hotmail.com>
2025-01-04 12:47:28 +0800

    Added parameer Jira Number and its verification function

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 use rcli::{process_csv, Opts, Subcommand};
 
 fn main() -> anyhow::Result<()> {
-    let opts: Opts = Opts::parse();
+    let opts = Opts::parse();
     match opts.cmd {
         Subcommand::Csv(opts) => process_csv(&opts.input, &opts.output)?,
     }
--- src/opts.rs
+++ src/opts.rs
@@ -4,6 +4,10 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 #[command(name = "rcli", version, author, about, long_about=None)]
 pub struct Opts {
+    // Example of Jira number: "GDTA-1234"
+    #[arg(value_parser = verify_jira_number)]
+    pub jira_number: String,
+
     #[command(subcommand)]
     pub cmd: Subcommand,
 
@@ -35,5 +39,13 @@ fn verify_input_file(filename: &str) -> Result<String, &'static str> {
         Ok(filename.into())
     } else {
         Err("File does not exist")
+    }
+}
+
+fn verify_jira_number(jira_number: &str) -> Result<String, &'static str> {
+    if jira_number.starts_with("GDTA-") {
+        Ok(jira_number.into())
+    } else {
+        Err("Jira number must start with 'GDTA-'")
     }
 }
